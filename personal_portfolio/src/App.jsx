c8679853d2d3dd5401a6f8dc0734a923b72de9a9
Mohamed <justmoe23@gmail.com>
2022-07-17 18:00:23 -0400

    create css variables and general styles

--- personal_portfolio/src/App.jsx
+++ personal_portfolio/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Project from './components/Project/Project'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <Navbar />
+    <About />
+    <Project />
+    <Contact />
+    <Footer />
+    </>
   )
 }
 
