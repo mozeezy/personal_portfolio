2e41436cb1b0fc20b989e36bce534ea94c8f6d14
Mohamed <justmoe23@gmail.com>
2022-07-17 18:28:20 -0400

    start header component

--- personal_portfolio/src/components/Header/Header.jsx
+++ personal_portfolio/src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HEADERIMG from '../../assets/header-img.jpeg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5> Hello I'm</h5>
+        <h1> Mohamed Mohamed</h1>
+        <h5 className="text_light"> Fullstack Web Developer</h5>
+        <CTA />
+
+        <div className="header-img">
+          <img src={HEADERIMG} alt="HEADERIMG" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
---
+++ personal_portfolio/src/components/Header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/mohamed-mohamed96/" target="_blank"></a>
+      <a href="https://www.github/mozeezy/" target="_blank"></a>
+      <a href="mailto: justmoe23@gmail.com" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
