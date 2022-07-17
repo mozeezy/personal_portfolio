072f1abd587059e10a319e4c782f6bae8b0b4092
Mohamed <justmoe23@gmail.com>
2022-07-17 19:11:54 -0400

    finish header component

--- personal_portfolio/src/components/Header/Header.jsx
+++ personal_portfolio/src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import HEADERIMG from '../../assets/header-img.jpeg'
+import HEADERIMG from '../../assets/avatar.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
         <h5> Hello I'm</h5>
         <h1> Mohamed Mohamed</h1>
         <h5 className="text_light"> Fullstack Web Developer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="header-img">
           <img src={HEADERIMG} alt="HEADERIMG" />
