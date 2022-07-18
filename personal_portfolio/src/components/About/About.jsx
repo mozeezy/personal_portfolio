d6e699abddbf663945d74a3bf8d48a4b3616b497
Mohamed <justmoe23@gmail.com>
2022-07-17 20:29:22 -0400

    finish about component

--- personal_portfolio/src/components/About/About.jsx
+++ personal_portfolio/src/components/About/About.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './about.css'
+import {BsThreeDots} from 'react-icons/bs'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5> Get To Know More</h5>
+      <h2> About Me</h2>
+
+      <div className="container about__container">
+        <p>
+          After an unsuccessful bout of landing a job after graduating, I figured that it was time for a career switch. That is when I stumbled upon an opportunity to learn about web development and coding at Lighthouse Labs.
+        </p>
+        <br/>
+        <BsThreeDots />
+        <br />
+        <p>Through my journey at Lighthouse Labs, I have built applications using various languages, libraries and tools such as JavaScript, HTML, CSS,SQL, Node.js, Express, React.js, jQuery, AJAX, PostgreSQL, and I am continuing to learn more. I'm currently enrolled in the Harvard CS50 course where I will be expanding on my coding skills!</p>
+        <br />
+        <BsThreeDots />
+        <br />
+        <p> Outside of coding I enjoy cooking, watching anime, listening to music, going to the gym, and reading. Fun fact: I also make beats and instrumentals. If you have the time, just search "Mozeezy" on your streaming platform of choice and listen away! 
+        </p>
+        <br />
+      </div>
+    </section>
   )
 }
 
