f2dfac7a06c28725ef6e3bae55e704278ddd4ce8
Mohamed <justmoe23@gmail.com>
2022-07-17 21:10:29 -0400

    start projects section

--- personal_portfolio/src/components/Project/Project.jsx
+++ personal_portfolio/src/components/Project/Project.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './project.css'
 
+
 const Project = () => {
   return (
-    <section id="projects">Projects</section>
+    <section id="projects">
+      <h5>Check out some of my</h5>
+      <h2>Projects</h2>
+    </section>
   )
 }
 
---
+++ personal_portfolio/src/projects/projects.jsx
@@ -0,0 +1,30 @@
+const project = [
+  {
+    id: 1,
+    title: "CryptoNite",
+    description: "A mock cryptocurrency app that allows you to trade different cryptocurrencies and access news related to crypto.",
+    image: "/projectImages/cryptonite.png",
+    githubURL: "https://github.com/mozeezy/cryptonite"
+  },
+  {
+    id: 2, 
+    title: "FoodSkip",
+    description: "A food pickup app that allows user to place orders for pickup. Similar to SkipTheDishes.",
+    image: "/projectImages/foodskip.png",
+    githubURL: "https://github.com/RReiso/foodskip"
+  },
+  {
+    id: 3,
+    title: "Interview Scheduler",
+    description: "An app that allows you to set appointments with interviewers",
+    image: "/projectImages/interview-scheduler.png",
+    githubURL: "https://github.com/mozeezy/scheduler"
+  },
+  {
+    id: 4,
+    title: "Tweeter",
+    description: "A mock version of Twitter that allows you to tweet within 140 characters",
+    image: "/projectImages/tweeter.png",
+    githubURL: "https://github.com/mozeezy/tweeter"
+  }
+]
