8b9c9ca95bee145e1809347db56cccd1aedb25af
Mohamed <justmoe23@gmail.com>
2022-12-15 17:11:25 -0500

    remove resume

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import CV from '../../assets/resume.pdf'
+
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href= {CV} rel="noreferrer" target="_blank" className='btn'>Browse My Resume</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
