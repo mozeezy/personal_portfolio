84757f3e496ddefe8a792afc60e317eefdec8cf5
Mohamed <justmoe23@gmail.com>
2023-01-03 21:58:09 -0500

    check that pull is successful

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Project from './components/Project/Project'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
+
 const App = () => {
   return (
     <>
