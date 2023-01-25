f036a1cd060d71a6ac1e47e9c565dfa77b1feb17
Mohamed <justmoe23@gmail.com>
2023-01-24 21:56:31 -0500

    add attribute to open resume in new tab

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -5,7 +5,7 @@ import resume from "../../assets/resume.pdf"
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={resume} className='btn'>Browse Resume</a>
+      <a href={resume} className='btn' target="_blank">Browse Resume</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
